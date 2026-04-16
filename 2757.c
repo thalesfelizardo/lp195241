81b13d4c6d3265ed00aff45cddda7c1e90ed5147
thalesfelizardo <thales.felizardo@unifesp.br>
2026-04-15 22:47:46 -0300

    Create 2757.c

---
+++ 2757.c
@@ -0,0 +1,25 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Linguagem   : C
+Plataforma  : https://judge.beecrowd.com/pt/search
+Exercício   : 2757
+Data        : 09/04/2026
+Objetivo    : Ler e imprimir numeros inteiros na tela.
+Aprendizado : Foi aprendido usar espaçamentos ao usar o comando printf.
+-------------------------------------------------------------------------- */
+
+#include <stdio.h>
+
+int main()
+{
+    int A, B, C;
+    
+    scanf("%d %u %u", &A, &B, &C);
+    
+    printf("A = %d, B = %u, C = %u\n", A, B, C);
+    printf("A = % 10d, B = % 10d, C = % 10d\n", A, B, C);
+    printf("A = %010d, B = %010d, C = %010d\n", A, B, C);
+    printf("A = %-10d, B = %-10d, C = %-10d\n", A, B, C);
+    
+    return 0;
+}
