414db536a69d0d9985f8d5cd49fd074e3dc2f141
thalesfelizardo <thales.felizardo@unifesp.br>
2026-04-15 22:42:26 -0300

    Create 1000.c

---
+++ 1000.c
@@ -0,0 +1,18 @@
+* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Linguagem   : C
+Plataforma  : https://judge.beecrowd.com/pt/search
+Exercício   : 1000
+Data        : 07/04/2026
+Objetivo    : imprimir uma frase na tela
+Aprendizado : comando printf
+-------------------------------------------------------------------------- */
+
+#include <stdio.h>
+ 
+int main() {
+ 
+    printf("Hello World!\n");
+  
+    return 0;
+}
