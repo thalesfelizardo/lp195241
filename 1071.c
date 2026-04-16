2db2bdf5298889694d015d8b8671c125e9a31f95
thalesfelizardo <thales.felizardo@unifesp.br>
2026-04-15 22:55:59 -0300

    Create 1071.c

---
+++ 1071.c
@@ -0,0 +1,35 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Linguagem   : C
+Plataforma  : https://judge.beecrowd.com/pt/search
+Exercício   : 1071
+Data        : 14/04/2026
+Objetivo    : Ler dois números inteiros e imprimir a soma dos números impares consecutivos entre eles.
+Aprendizado : Foi aprendido o uso do laço de repetição 'for' e o uso do comando "%" para saber o resto da divisão.
+-------------------------------------------------------------------------- */
+
+
+#include <stdio.h>
+
+int main(){
+
+    int x, y;
+    int i = 0;
+    int soma = 0;
+
+    scanf("%d %d", &x, &y);
+    
+        for(i = y + 1; i < x; i++)
+        {
+            if (i % 2 != 0) 
+            {
+                soma += i;
+            }
+        
+        }
+
+
+    printf("%d\n", soma);
+
+    return 0;
+}
