701faa846b3d41d05912ba2902e8bdebd1cf92e7
thalesfelizardo <thales.felizardo@unifesp.br>
2026-04-20 23:23:14 -0300

    Update 1000.c

--- 1000.c
+++ 1000.c
@@ -1,8 +1,8 @@
 /* --------------------------------------------------------------------------
 Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : THALES FELIZARDO MAIA
 Linguagem   : C
-Plataforma  : https://judge.beecrowd.com/pt/search
-Exercício   : 1000
+Problema    : https://judge.beecrowd.com/pt/problems/view/1000
 Data        : 07/04/2026
 Objetivo    : imprimir uma frase na tela
 Aprendizado : comando printf
