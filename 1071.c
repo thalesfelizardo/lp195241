d8b0ea859903eef18b0cfc18cf1729c2c5c3d164
thalesfelizardo <thales.felizardo@unifesp.br>
2026-04-20 23:22:25 -0300

    Update 1071.c

--- 1071.c
+++ 1071.c
@@ -1,8 +1,8 @@
 /* --------------------------------------------------------------------------
 Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : THALES FELIZARDO MAIA
 Linguagem   : C
-Plataforma  : https://judge.beecrowd.com/pt/search
-Exercício   : 1071
+Problema    : https://judge.beecrowd.com/pt/problems/view/1080
 Data        : 14/04/2026
 Objetivo    : Ler dois números inteiros e imprimir a soma dos números impares consecutivos entre eles.
 Aprendizado : Foi aprendido o uso do laço de repetição 'for' e o uso do comando "%" para saber o resto da divisão.
