3afee6743e25a08ede3961df1367b2b1bb3960bc
thalesfelizardo <thales.felizardo@unifesp.br>
2026-04-28 16:49:41 -0300

    Update 1078.c

--- 1078.c
+++ 1078.c
@@ -28,22 +28,3 @@ int main() {
     return 0;
 }
 
-#include <stdio.h>
- 
-int main() {
-    
-    int n, i;
-    
-    scanf("%d", &n);
-    
-    if(n < 2 || n > 1000){
-        return -1;
-    }
-    
-    
-    for(i = 1; i < 11; i++){
-        printf("%d x %d = %d\n", i, n, n*i);
-    }
- 
-    return 0;
-}
