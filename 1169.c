f3354e048806f639cc54946d011343cc11f42465
thalesfelizardo <thales.felizardo@unifesp.br>
2026-06-24 00:54:22 -0300

    Create 1169.c

---
+++ 1169.c
@@ -0,0 +1,38 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : THALES FELIZARDO MAIA
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/2758
+Data        : 09/04/2026
+Objetivo    : imprimir quantos kg de graos foram coletados de acordo com as casas do tabuleiro
+Aprendizado : uso do unsigned long long 
+-------------------------------------------------------------------------- */
+
+
+#include <stdio.h>
+
+unsigned long long graos(int casa) {
+
+    if (casa == 1) {
+        return 1;
+    }
+
+    return 2 * graos(casa - 1) + 1;
+}
+
+int main() {
+
+    int casos;
+
+    scanf("%d", &casos);
+
+    for (int i = 0; i < casos; i++) {
+
+        int casa;
+        scanf("%d", &casa);
+
+        printf("%llu kg\n", graos(casa)/12000);
+    }
+
+    return 0;
+}
